5aa06a30774772cfe8d6eeac0114d357076d2675
Darshan Choudhary <97291483+DarshanchGIT@users.noreply.github.com>
2024-07-07 21:41:37 +0530

    just added my self made snippet

---
+++ darshan.js
@@ -0,0 +1,7 @@
+function measureExecutionTime(callback) {
+  const before = new Date().getTime();
+  callback();
+  const after = new Date().getTime();
+  const timeTook = (after - before) / 1000;
+  console.log("Time it took in seconds: " + timeTook);
+}
