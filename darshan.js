4f4773772f45d156ee9e2309c8674cf20fda2b6e
Darshan Choudhary <97291483+DarshanchGIT@users.noreply.github.com>
2024-07-07 21:42:33 +0530

    Update darshan.js

--- darshan.js
+++ darshan.js
@@ -1,6 +1,6 @@
 function measureExecutionTime(callback) {
   const before = new Date().getTime();
-  callback();
+  // callback();
   const after = new Date().getTime();
   const timeTook = (after - before) / 1000;
   console.log("Time it took in seconds: " + timeTook);
